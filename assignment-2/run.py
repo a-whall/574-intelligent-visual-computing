3a65d3bf43f1a07bfa3fd4b111c6972bceebcdb7
Andrew Hall <awh577868@gmail.com>
2023-06-12 19:29:43 -0400

    add cuda check to automatically enable gpu acceleration

--- assignment-2/run.py
+++ assignment-2/run.py
@@ -6,13 +6,18 @@ import torch
 train_path = './dataset/train'
 test_path = './dataset/test'
 
+use_cuda = torch.cuda.is_available()
+
 # TRAIN
-model, info = trainMVShapeClassifier(train_path, cuda=False, verbose=False)
-#
-# # TO SAVE TIME for just testing code, uncomment the following 2 lines to load your pre-trained model
+model, info = trainMVShapeClassifier(train_path, cuda=use_cuda, verbose=False)
+
+# TO SAVE TIME
+# -------------------------------------------------------------------------------------------------------
+# For testing a pre-trained model, replace the training function above with the following 2 lines of code
+# -------------------------------------------------------------------------------------------------------
 # model = torch.load('model/model_epoch_19.pth', map_location=lambda storage, location: storage)["model"]
 # info = p.load( open( "info.p", "rb" ) )
 
 # TEST
-testMVImageClassifier(test_path, model, info, pooling='mean', cuda=False, verbose=False)
-testMVImageClassifier(test_path, model, info, pooling='max', cuda=False, verbose=False)
+testMVImageClassifier(test_path, model, info, pooling='mean', cuda=use_cuda, verbose=False)
+testMVImageClassifier(test_path, model, info, pooling='max', cuda=use_cuda, verbose=False)
