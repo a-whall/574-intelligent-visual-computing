abd225540b3aaf396b21988f3f60b3cfce25928c
Andrew Hall <awh577868@gmail.com>
2023-06-13 22:43:57 -0400

    implement task A, single-deepsdf model architecture

--- assignment-4/model.py
+++ assignment-4/model.py
@@ -1,34 +1,29 @@
-import torch.nn as nn
 import torch
-import torch.nn.functional as F
+from torch.nn import Module, Sequential as Seq, Linear as Lin, Dropout, PReLU, Tanh
+from torch.nn.utils import weight_norm
 
-
-class Decoder(nn.Module):
-    def __init__(
-        self,
-        args,
-        dropout_prob=0.1,
-    ):
+class Decoder(Module):
+    def __init__(self, args, dropout_prob=0.1,):
         super(Decoder, self).__init__()
+        self.dropout = Dropout(p=dropout_prob)
+        self.fc1 = Seq(weight_norm(Lin(  3, 512)), PReLU(), self.dropout)
+        self.fc2 = Seq(weight_norm(Lin(512, 512)), PReLU(), self.dropout)
+        self.fc3 = Seq(weight_norm(Lin(512, 512)), PReLU(), self.dropout)
+        self.fc4 = Seq(weight_norm(Lin(512, 509)), PReLU(), self.dropout)
+        self.fc5 = Seq(weight_norm(Lin(512, 512)), PReLU(), self.dropout)
+        self.fc6 = Seq(weight_norm(Lin(512, 512)), PReLU(), self.dropout)
+        self.fc7 = Seq(weight_norm(Lin(512, 512)), PReLU(), self.dropout)
+        self.fc8 = Seq(Lin(512, 1), Tanh())
 
-        # **** YOU SHOULD IMPLEMENT THE MODEL ARCHITECTURE HERE ****
-        # Define the network architecture based on the figure shown in the assignment page.
-        # Read the instruction carefully for layer details.
-        # Pay attention that your implementation should include FC layers, weight_norm layers,
-        # PReLU layers, Dropout layers and a tanh layer.
-        self.fc = nn.Linear(3, 1)
-        self.dropout_prob = dropout_prob
-        self.th = nn.Tanh()
-        # ***********************************************************************
-
-    # input: N x 3
-    def forward(self, input):
-
-        # **** YOU SHOULD IMPLEMENT THE FORWARD PASS HERE ****
-        # Based on the architecture defined above, implement the feed forward procedure
-        x = self.fc(input)
-        x = self.th(x)
-        # ***********************************************************************
-
-
+    def forward(self, x):
+        x_in = x
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.fc3(x)
+        x = self.fc4(x)
+        x = torch.cat([x, x_in], dim=1)
+        x = self.fc5(x)
+        x = self.fc6(x)
+        x = self.fc7(x)
+        x = self.fc8(x)
         return x
